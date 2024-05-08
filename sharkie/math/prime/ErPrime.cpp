97f3f948ab4917ff669e3436372e1cdad480a6b0
salmonfishycooked <salmonfishycooked@gmail.com>
2024-05-09 07:14:08 +0800

    add Eratosthenes Sieve

---
+++ sharkie/math/prime/ErPrime.cpp
@@ -0,0 +1,33 @@
+/**
+ * 埃式筛
+ *
+ * 方法说明：
+ * 1. void cal(int n) 计算 1 ~ n 之间的所有素数，将结果有序存储在 prime 数组里面。
+ *
+ * 时间复杂度：O(nlog(logn))
+ */
+
+#include <bits/stdc++.h>
+
+class ErPrime {
+public:
+    std::vector<int> prime;
+
+    void cal(int n) {
+        prime.resize(0);
+        if (n >= 2) {
+            prime.push_back(2);
+        }
+
+        std::vector<bool> vis(n + 1);
+        for (int i = 3; i <= n; i += 2) {
+            if (vis[i]) { continue; }
+            prime.push_back(i);
+            int next = i + i;
+            while (next <= n) {
+                vis[next] = true;
+                next += i;
+            }
+        }
+    }
+};
