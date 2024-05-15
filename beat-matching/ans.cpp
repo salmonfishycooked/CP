dae02535b1252b868b11d671df7348e833a4f15a
salmonfishycooked <salmonfishycooked@gmail.com>
2024-05-15 12:55:14 +0800

    add judge(beat matching) template

---
+++ beat-matching/ans.cpp
@@ -0,0 +1,4 @@
+/**
+ * there should be a absolutely right code for the problem
+ * you should also reopen stdin and stdout to make it works.
+ */
---
+++ beat-matching/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using i64 = int64_t;
+
+void solve() {
+
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    freopen("e.in", "r", stdin);
+    freopen("main.out", "w", stdout);
+
+    solve();
+
+    return 0;
+}
