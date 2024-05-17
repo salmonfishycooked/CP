200baf47295821cb4ed240758d0b1b92be93cc07
salmonfishycooked <salmonfishycooked@gmail.com>
2024-05-17 21:24:14 +0800

    add luogu P3627

---
+++ luogu/problem/P3627/P3627.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+
+using i64 = int64_t;
+
+class SCC {
+public:
+    int n;
+    std::vector<std::vector<int>> adj;
+
+    std::vector<std::vector<int>> scc;
+    std::vector<int> belong;
+
+    explicit SCC(int _n) : n(_n) {
+        adj.assign(n + 1, {});
+        belong.resize(n + 1);
+    }
+
+    void addEdge(int u, int v) {
+        adj[u].push_back(v);
+    }
+
+    void tarjan() {
+        int timestamp = 0;
+        std::vector<int> dfn(n + 1), low(n + 1);
+        std::vector<bool> inStack(n + 1);
+        std::stack<int> stk;
+
+        auto dfs = [&](auto self, int u) -> void {
+            dfn[u] = low[u] = ++timestamp;
+            inStack[u] = true;
+            stk.push(u);
+
+            for (auto v : adj[u]) {
+                if (!dfn[v]) { self(self, v); }
+                if (inStack[v]) {
+                    low[u] = std::min(low[u], low[v]);
+                }
+            }
+
+            if (dfn[u] == low[u]) {
+                std::vector<int> cur;
+                while (true) {
+                    int v = stk.top();
+                    stk.pop();
+                    cur.push_back(v);
+                    inStack[v] = false;
+                    belong[v] = scc.size();
+                    if (v == u) { break; }
+                }
+                scc.push_back(cur);
+            }
+        };
+
+        for (int i = 1; i <= n; i++) {
+            if (!dfn[i]) { dfs(dfs, i); }
+        }
+    }
+};
+
+void solve() {
+    int n, m;
+    std::cin >> n >> m;
+
+    SCC scc(n);
+    for (int i = 0, u, v; i < m; i++) {
+        std::cin >> u >> v;
+        scc.addEdge(u, v);
+    }
+
+    scc.tarjan();
+
+    std::vector<i64> money(scc.scc.size());
+    for (i64 i = 1, tmp; i <= n; i++) {
+        std::cin >> tmp;
+        money[scc.belong[i]] += tmp;
+    }
+
+    int start, barCnt;
+    std::cin >> start >> barCnt;
+    std::vector<bool> bar(scc.scc.size());
+    for (int i = 0, id; i < barCnt; i++) {
+        std::cin >> id;
+        bar[scc.belong[id]] = true;
+    }
+
+    std::vector<i64> f(scc.scc.size());
+    for (int i = 0; i < scc.scc.size(); i++) {
+        f[i] = LONG_LONG_MIN;
+        if (bar[i]) { f[i] = money[i]; }
+        for (auto source : scc.scc[i]) {
+            for (auto to : scc.adj[source]) {
+                if (scc.belong[to] == i) { continue; }
+                f[i] = std::max(f[i], f[scc.belong[to]] + money[i]);
+            }
+        }
+    }
+
+    std::cout << f[scc.belong[start]] << '\n';
+};
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
