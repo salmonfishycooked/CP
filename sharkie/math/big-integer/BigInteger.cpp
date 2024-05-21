77498583a7ab491fcd745cb04cd1d2ca6b560b95
salmonfishycooked <salmonfishycooked@gmail.com>
2024-05-21 14:07:13 +0800

    update BigInteger

--- sharkie/math/big-integer/BigInteger.cpp
+++ sharkie/math/big-integer/BigInteger.cpp
@@ -1,7 +1,10 @@
 /**
  * 高精度整数 - BigInteger
+ * 支持负数
+ * 支持 +, -, *, /, 取余，取模运算
  *
- * 目前只支持非负数的运算
+ * Note: 其中 * 运算还未进行优化，目前时间复杂度为 O(m * n)，其中 m 与 n 分别为两个操作数的数位长度。
+ *       其中 / 运算用到了 * 运算，所以其时间复杂度也未被优化。
  */
 
 #include <bits/stdc++.h>
@@ -10,15 +13,27 @@ using i64 = int64_t;
 
 class BigInteger {
 public:
-    static constexpr int BASE = 10;
+    static constexpr int BASE = 100000000;
+    static constexpr int IO_WIDTH = 8;
+
+    char sign;
+    constexpr static int SIGN_POSITIVE = 1;
+    constexpr static int SIGN_NEGATIVE = -1;
 
     std::vector<int> s;
 
     BigInteger(i64 num = 0) {
+        sign = SIGN_POSITIVE;
         *this = num;
     }
 
     BigInteger &operator=(i64 num) {
+        sign = SIGN_POSITIVE;
+        if (num < 0) {
+            sign = SIGN_NEGATIVE;
+            num = -num;
+        }
+
         s.clear();
         do {
             s.push_back(num % BASE);
@@ -29,24 +44,251 @@ public:
     }
 
     BigInteger &operator=(const std::string &str) {
+        if (str.empty()) {
+            return *this;
+        }
+
+        int startIdx = 0;
+        if (str[0] == '-') {
+            sign = SIGN_NEGATIVE;
+            startIdx = 1;
+        }
+
         s.clear();
-        for (int i = str.size() - 1; i >= 0; i--) {
-            s.push_back(str[i] - '0');
+        for (int i = str.size() - 1; i >= startIdx; ) {
+            int j = i - IO_WIDTH + 1;
+            if (j >= startIdx) {
+                s.push_back(std::atoi(str.substr(j, IO_WIDTH).c_str()));
+                i -= IO_WIDTH;
+                continue;
+            }
+            s.push_back(std::atoi(str.substr(startIdx, i - startIdx + 1).c_str()));
+            break;
         }
 
         return *this;
     }
 
-    BigInteger operator+(const BigInteger &b) {
+    BigInteger operator+(const BigInteger &b) const {
+        BigInteger ret;
+        if (sign == SIGN_NEGATIVE && b.sign == SIGN_NEGATIVE || sign == SIGN_POSITIVE && b.sign == SIGN_POSITIVE) {
+            ret = plusNoSign(*this, b);
+            ret.sign = sign;
+            return ret;
+        }
+        if (sign == SIGN_NEGATIVE && b.sign == SIGN_POSITIVE) {
+            BigInteger tt = *this;
+            tt.sign = SIGN_POSITIVE;
+            return b - tt;
+        }
+        if (sign == SIGN_POSITIVE && b.sign == SIGN_NEGATIVE) {
+            BigInteger tt = b;
+            tt.sign = SIGN_POSITIVE;
+            return *this - tt;
+        }
+
+        return ret;
+    }
+
+    BigInteger &operator+=(const BigInteger &b) {
+        *this = *this + b;
+
+        return *this;
+    }
+
+    BigInteger operator-(const BigInteger &b) const {
+        BigInteger ret;
+        if (sign == SIGN_POSITIVE && b.sign == SIGN_POSITIVE) {
+            if (*this > b) {
+                ret = subNoSign(*this, b);
+                return ret;
+            }
+            ret = subNoSign(b, *this);
+            ret.sign = SIGN_NEGATIVE;
+            return ret;
+        }
+        if (sign == SIGN_NEGATIVE && b.sign == SIGN_NEGATIVE) {
+            if (b < *this) {
+                ret = subNoSign(b, *this);
+                return ret;
+            }
+            ret = subNoSign(*this, b);
+            ret.sign = SIGN_NEGATIVE;
+            return ret;
+        }
+        if (sign == SIGN_POSITIVE && b.sign == SIGN_NEGATIVE) {
+            ret = plusNoSign(*this, b);
+            return ret;
+        }
+        if (sign == SIGN_NEGATIVE && b.sign == SIGN_POSITIVE) {
+            ret = plusNoSign(*this, b);
+            ret.sign = SIGN_NEGATIVE;
+            return ret;
+        }
+
+        return ret;
+    }
+
+    BigInteger &operator-=(const BigInteger &b) {
+        *this = *this - b;
+
+        return *this;
+    }
+
+    BigInteger operator*(const BigInteger &b) const {
+        BigInteger ret = mulNoSign(*this, b);
+        if (sign == SIGN_POSITIVE && b.sign == SIGN_NEGATIVE || sign == SIGN_NEGATIVE && b.sign == SIGN_POSITIVE) {
+            ret.sign = SIGN_NEGATIVE;
+        }
+
+        return ret;
+    }
+
+    BigInteger &operator*=(const BigInteger &b) {
+        *this = *this * b;
+
+        return *this;
+    }
+
+    BigInteger operator/(const BigInteger &b) const {
+        assert(b != 0);
+
+        BigInteger ret = divNoSign(*this, b);
+        if (sign == SIGN_POSITIVE && b.sign == SIGN_NEGATIVE || sign == SIGN_NEGATIVE && b.sign == SIGN_POSITIVE) {
+            ret.sign = SIGN_NEGATIVE;
+        }
+
+        return ret;
+    }
+
+    BigInteger &operator/=(const BigInteger &b) {
+        *this = *this / b;
+
+        return *this;
+    }
+
+    BigInteger remainder(const BigInteger &val) const {
+        BigInteger t1 = *this / val;
+        BigInteger t2 = t1 * val;
+        BigInteger t3 = *this - t2;
+        return t3;
+    }
+
+    BigInteger mod(const BigInteger &m) const {
+        BigInteger result = this->remainder(m);
+
+        return (result.sign >= 0 ? result : result + m);
+    }
+
+    bool operator==(const BigInteger &b) const {
+        if (s.size() == 1 && s[0] == 0 && b.s.size() == 1 && b.s[0] == 0) {
+            return true;
+        }
+        if (sign != b.sign || s.size() != b.s.size()) {
+            return false;
+        }
+
+        for (int i = s.size() - 1; i >= 0; i--) {
+            if (s[i] != b.s[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool operator>(const BigInteger &b) const {
+        if (s.size() == 1 && s[0] == 0 && b.s.size() == 1 && b.s[0] == 0) {
+            return false;
+        }
+
+        if (sign < 0 && b.sign > 0) {
+            return false;
+        }
+        if (sign > 0 && b.sign < 0) {
+            return true;
+        }
+        if (sign > 0 && b.sign > 0) {
+            if (s.size() != b.s.size()) {
+                return s.size() > b.s.size();
+            }
+            for (int i = s.size() - 1; i >= 0; i--) {
+                if (s[i] != b.s[i]) {
+                    return s[i] > b.s[i];
+                }
+            }
+        }
+        if (sign < 0 && b.sign < 0) {
+            if (s.size() != b.s.size()) {
+                return s.size() < b.s.size();
+            }
+            for (int i = s.size() - 1; i >= 0; i--) {
+                if (s[i] != b.s[i]) {
+                    return s[i] < b.s[i];
+                }
+            }
+        }
+
+        return false;
+    }
+
+    bool operator>=(const BigInteger &b) const {
+        return *this > b || *this == b;
+    }
+
+    bool operator<(const BigInteger &b) const {
+        return !(*this >= b);
+    }
+
+    bool operator<=(const BigInteger &b) const {
+        return !(*this > b);
+    }
+
+    bool operator!=(const BigInteger &b) const {
+        return !(*this == b);
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const BigInteger &x) {
+        if (x.s.size() == 1 && x.s[0] == 0) {
+            out << 0;
+            return out;
+        }
+
+        if (x.sign == -1) {
+            std::cout << '-';
+        }
+
+        out << x.s[x.s.size() - 1];
+        for (int i = x.s.size() - 2; i >= 0; i--) {
+            out << std::setw(IO_WIDTH) << std::setfill('0') << x.s[i];
+        }
+
+        return out;
+    }
+
+    friend std::istream &operator>>(std::istream &in, BigInteger &x) {
+        x.sign = SIGN_POSITIVE;
+        std::string str;
+        in >> str;
+        x = str;
+
+        return in;
+    }
+private:
+    /**
+     * plusNoSign calculates the a + b, where a >= 0, b >= 0 (means without sign).
+     * without sign means this function will ignore their sign (a and b)
+     */
+    static BigInteger plusNoSign(const BigInteger &a, const BigInteger &b) {
         BigInteger ret;
         ret.s.clear();
 
         for (int i = 0, exp = 0; ; i++) {
-            if (exp == 0 && i >= s.size() && i >= b.s.size()) { break; }
+            if (exp == 0 && i >= a.s.size() && i >= b.s.size()) { break; }
 
             int x = exp;
-            if (i < s.size()) {
-                x += s[i];
+            if (i < a.s.size()) {
+                x += a.s[i];
             }
             if (i < b.s.size()) {
                 x += b.s[i];
@@ -58,13 +300,44 @@ public:
         return ret;
     }
 
-    BigInteger operator+=(const BigInteger &b) {
-        *this = *this + b;
+    /**
+     * subNoSign calculates the a - b, where a >= 0, b >= 0 (means without sign).
+     * without sign means this function will ignore their sign (a and b)
+     * Note: a needs to be greater or equal to b!
+     */
+    static BigInteger subNoSign(const BigInteger &a, const BigInteger &b) {
+        BigInteger ret = a;
+        ret.sign = SIGN_POSITIVE;
 
-        return *this;
+        for (int i = 0, exp = 0; ; i++) {
+            if (exp == 0 && i >= b.s.size()) { break; }
+
+            i64 subNum = exp;
+            exp = 0;
+            if (i < b.s.size()) {
+                subNum += b.s[i];
+            }
+            if (ret.s[i] < subNum) {
+                exp = 1;
+                ret.s[i] += BASE;
+            }
+            ret.s[i] -= subNum;
+        }
+
+        while (ret.s.size() > 1 && ret.s[ret.s.size() - 1] == 0) {
+            ret.s.pop_back();
+        }
+
+        return ret;
     }
 
-    BigInteger operator*(const BigInteger &b) {
+    /**
+     * mulNoSign calculates the a * b, where a >= 0, b >= 0 (means without sign).
+     * without sign means this function will ignore their sign (a and b)
+     */
+    static BigInteger mulNoSign(const BigInteger &a, const BigInteger &b) {
+        if (a == 0 || b == 0) { return 0; }
+
         BigInteger ret = 0;
         for (int i = 0; i < b.s.size(); i++) {
             BigInteger cur;
@@ -73,9 +346,9 @@ public:
                 cur.s.push_back(0);
             }
 
-            int op = b.s[i], exp = 0;
-            for (int digit : s) {
-                int x = exp + digit * op;
+            i64 op = b.s[i], exp = 0;
+            for (i64 digit : a.s) {
+                i64 x = exp + digit * op;
                 cur.s.push_back(x % BASE);
                 exp = x / BASE;
             }
@@ -90,25 +363,39 @@ public:
         return ret;
     }
 
-    BigInteger &operator*=(const BigInteger &b) {
-        *this = *this * b;
+    /**
+     * divNoSign calculates the a / b, where a >= 0, b >= 0 (means without sign).
+     * without sign means this function will ignore their sign (a and b)
+     */
+    static BigInteger divNoSign(BigInteger a, BigInteger b) {
+        a.sign = SIGN_POSITIVE, b.sign = SIGN_POSITIVE;
+        BigInteger ret;
+        ret.s.clear();
 
-        return *this;
-    }
+        BigInteger cur = 0;
+        for (int i = a.s.size() - 1; i >= 0; i--, cur *= BASE) {
+            cur += a.s[i];
+            if (cur < b) { continue; }
 
-    friend std::ostream &operator<<(std::ostream &out, const BigInteger &x) {
-        for (int i = x.s.size() - 1; i >= 0; i--) {
-            out << x.s[i];
+            int l = 0, r = BASE;
+            while (l + 1 != r) {
+                int mid = (l + r) >> 1;
+                if (b * mid <= cur) {
+                    l = mid;
+                } else {
+                    r = mid;
+                }
+            }
+            cur -= b * l;
+            ret.s.push_back(l);
         }
 
-        return out;
-    }
+        if (ret.s.empty()) {
+            ret.s.push_back(0);
+        }
 
-    friend std::istream &operator>>(std::istream &in, BigInteger &x) {
-        std::string str;
-        in >> str;
-        x = str;
+        std::reverse(ret.s.begin(), ret.s.end());
 
-        return in;
+        return ret;
     }
 };
