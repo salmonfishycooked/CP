b797ffe273c400ec153671f5f3928d9333120c86
salmonfishycooked <salmonfishycooked@gmail.com>
2024-06-01 11:19:01 +0800

    update hdu 1754

--- hduoj/1754/1754.cpp
+++ hduoj/1754/1754.cpp
@@ -2,28 +2,26 @@
 
 using i64 = int64_t;
 
-template <typename T = int>
 class SegTree {
 public:
     struct Info {
         i64 v;
-
-        Info operator+(const Info &b) {
-            Info info;
-            info.v = std::max(v, b.v);
-
-            return info;
-        }
     };
-    std::vector<Info> seg;
 
-    explicit SegTree(std::vector<T> &a) {
-        int n = a.size();
+    struct Node {
+        int l, r;
+        Info info;
+    };
+    std::vector<Node> seg;
+
+    explicit SegTree(std::vector<i64> &a) {
+        auto n = a.size();
         seg.resize(n * 4);
 
         auto build = [&](auto self, int id, int l, int r) {
+            seg[id].l = l, seg[id].r = r;
             if (l == r) {
-                seg[id] = {a[l - 1]};
+                seg[id].info = {a[l - 1]};
                 return;
             }
 
@@ -36,37 +34,49 @@ public:
     }
 
     void update(int id) {
-        seg[id] = seg[id * 2] + seg[id * 2 + 1];
+        int lc = id * 2, rc = lc + 1;
+        seg[id].info = combineInfoAndInfo(seg[lc].info, seg[lc].l, seg[lc].r, seg[rc].info, seg[rc].l, seg[rc].r);
     }
 
-    void change(int id, int l, int r, int pos, T val) {
+    void change(int id, int pos, i64 val) {
+        int l = seg[id].l, r = seg[id].r;
         if (l == r) {
-            seg[id] = {val};
+            seg[id].info.v = val;
             return;
         }
 
         int mid = (l + r) >> 1;
         if (pos <= mid) {
-            change(id * 2, l, mid, pos, val);
+            change(id * 2, pos, val);
         } else {
-            change(id * 2 + 1, mid + 1, r, pos, val);
+            change(id * 2 + 1, pos, val);
         }
         update(id);
     }
 
-    Info query(int id, int l, int r, int ql, int qr) {
+    Info query(int id, int ql, int qr) {
+        int l = seg[id].l, r = seg[id].r;
         if (l == ql && r == qr) {
-            return seg[id];
+            return seg[id].info;
         }
 
         int mid = (l + r) >> 1;
         if (qr <= mid) {
-            return query(id * 2, l, mid, ql, qr);
+            return query(id * 2, ql, qr);
         } else if (ql > mid) {
-            return query(id * 2 + 1, mid + 1, r, ql, qr);
+            return query(id * 2 + 1, ql, qr);
         } else {
-            return query(id * 2, l, mid, ql, mid) + query(id * 2 + 1, mid + 1, r, mid + 1, qr);
+            Info info1 = query(id * 2, ql, mid), info2 = query(id * 2 + 1, mid + 1, qr);
+            return combineInfoAndInfo(info1, ql, mid, info2, mid + 1, qr);
         }
+    }
+
+private:
+    Info combineInfoAndInfo(const Info &info1, int l1, int r1, const Info &info2, int l2, int r2) {
+        Info info;
+        info.v = std::max(info1.v, info2.v);
+
+        return info;
     }
 };
 
@@ -76,16 +86,16 @@ void solve(int n, int m) {
         std::cin >> a[i];
     }
 
-    SegTree<i64> tree(a);
+    SegTree tree(a);
     while (m--) {
         char op;
         int x, y;
         std::cin >> op >> x >> y;
 
         if (op == 'Q') {
-            std::cout << tree.query(1, 1, n, x, y).v << '\n';
+            std::cout << tree.query(1, x, y).v << '\n';
         } else {
-            tree.change(1, 1, n, x, y);
+            tree.change(1, x, y);
         }
     }
 }
