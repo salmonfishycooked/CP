1afca966fc3fc821d77162bf21fc1f4a1980fe95
salmonfishycooked <salmonfishycooked@gmail.com>
2024-06-01 11:18:55 +0800

    update Seg Tree

--- sharkie/datastructure/seg-tree/SegTree.cpp
+++ sharkie/datastructure/seg-tree/SegTree.cpp
@@ -4,13 +4,19 @@
  * 数组元素编号：1~n，注意传入的数组元素编号是从 0 开始的，长度为 n。
  *
  * 方法说明：
- * 1. void change(int id, int l, int r, int pos, T val)，将 pos 位置的值修改为 val（或者是其他修改方法，比如增量修改），注意这里 pos 范围为 1~n。
- *    使用示例: change(1, 1, n, 5, 10)，就是从 id 为 1 的线段树节点开始查找（该线段树掌管 1~n 区间的信息），将位置为 5 的元素修改成对应值。
- * 2. Info query(int id, int l, int r, int ql, int qr)，查询区间 [ql, qr] 上的信息。
- *    使用示例: query(1, 1, n, 4, 10)，就是从 id 为 1 的线段树节点开始查找（该线段树掌管 1~n 区间的信息），返回区间 [4, 10] 的信息。
+ * 1. void change(int id, int pos, T val)，将 pos 位置的值修改为 val（或者是其他修改方法，比如增量修改），注意这里 pos 范围为 1~n。
+ *    使用示例: change(1, 5, 10)，就是从 id 为 1 的线段树节点开始查找（该线段树掌管 1~n 区间的信息，即树根），将位置为 5 的元素修改成对应值。
+ * 2. Info query(int id, int ql, int qr)，查询区间 [ql, qr] 上的信息。
+ *    使用示例: query(1, 4, 10)，就是从 id 为 1 的线段树节点开始查找（该线段树掌管 1~n 区间的信息，即树根），返回区间 [4, 10] 的信息。
  *
  * 参数说明：
- * 1. Info，即线段树的每个节点需要掌管的信息。其重载了+运算符，该运算里面是线段树的节点信息的更新逻辑。
+ * 1. Node，即线段树的节点，里面存了对应的 Info 以及其掌管的区域 [l, r]。
+ * 2. Info，即线段树的每个节点需要掌管的信息。
+ *
+ * 核心方法：
+ * 1. 节点信息合并：
+ *    Info combineInfoAndInfo(const Info &info1, int l1, int r1, const Info &info2, int l2, int r2)
+ *    该方法会合并 info1 和 info2 的信息，其中 [l1, r1] 是 info1 掌管的区间，[l2, r2] 是 info2 掌管的区间。
  *
  * 下面模板给出的是求区间和的示例。
  */
@@ -23,23 +29,22 @@ class SegTree {
 public:
     struct Info {
         i64 v;
-
-        Info operator+(const Info &b) {
-            Info info;
-            info.v = v + b.v;
-
-            return info;
-        }
     };
-    std::vector<Info> seg;
+
+    struct Node {
+        int l, r;
+        Info info;
+    };
+    std::vector<Node> seg;
 
     explicit SegTree(std::vector<i64> &a) {
-        int n = a.size();
+        auto n = a.size();
         seg.resize(n * 4);
 
         auto build = [&](auto self, int id, int l, int r) {
+            seg[id].l = l, seg[id].r = r;
             if (l == r) {
-                seg[id] = {a[l - 1]};
+                seg[id].info = {a[l - 1]};
                 return;
             }
 
@@ -52,36 +57,48 @@ public:
     }
 
     void update(int id) {
-        seg[id] = seg[id * 2] + seg[id * 2 + 1];
+        int lc = id * 2, rc = lc + 1;
+        seg[id].info = combineInfoAndInfo(seg[lc].info, seg[lc].l, seg[lc].r, seg[rc].info, seg[rc].l, seg[rc].r);
     }
 
-    void change(int id, int l, int r, int pos, i64 val) {
+    void change(int id, int pos, i64 val) {
+        int l = seg[id].l, r = seg[id].r;
         if (l == r) {
-            seg[id] = {val};
+            seg[id].info.v = val;
             return;
         }
 
         int mid = (l + r) >> 1;
         if (pos <= mid) {
-            change(id * 2, l, mid, pos, val);
+            change(id * 2, pos, val);
         } else {
-            change(id * 2 + 1, mid + 1, r, pos, val);
+            change(id * 2 + 1, pos, val);
         }
         update(id);
     }
 
-    Info query(int id, int l, int r, int ql, int qr) {
+    Info query(int id, int ql, int qr) {
+        int l = seg[id].l, r = seg[id].r;
         if (l == ql && r == qr) {
-            return seg[id];
+            return seg[id].info;
         }
 
         int mid = (l + r) >> 1;
         if (qr <= mid) {
-            return query(id * 2, l, mid, ql, qr);
+            return query(id * 2, ql, qr);
         } else if (ql > mid) {
-            return query(id * 2 + 1, mid + 1, r, ql, qr);
+            return query(id * 2 + 1, ql, qr);
         } else {
-            return query(id * 2, l, mid, ql, mid) + query(id * 2 + 1, mid + 1, r, mid + 1, qr);
+            Info info1 = query(id * 2, ql, mid), info2 = query(id * 2 + 1, mid + 1, qr);
+            return combineInfoAndInfo(info1, ql, mid, info2, mid + 1, qr);
         }
+    }
+
+private:
+    Info combineInfoAndInfo(const Info &info1, int l1, int r1, const Info &info2, int l2, int r2) {
+        Info info;
+        info.v = info1.v + info2.v;
+
+        return info;
     }
 };
