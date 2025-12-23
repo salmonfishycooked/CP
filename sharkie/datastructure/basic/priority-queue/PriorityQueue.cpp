13a07c01b52156f3856ae55879777c12be33011c
salmonfishycooked <salmonfishycooked@gmail.com>
2025-12-23 23:11:11 +0800

    modify: Priority Queue

--- sharkie/datastructure/basic/priority-queue/PriorityQueue.cpp
+++ sharkie/datastructure/basic/priority-queue/PriorityQueue.cpp
@@ -3,15 +3,17 @@
  * 支持按编号插入，按编号删除，按编号修改。
  *
  * 方法说明：
- * 1. void push(int id, const T &v)，将编号为 id，值为 v 的节点入堆。
- * 2. void pop()，弹出堆顶。
- * 3. void popById(int id)，弹出编号为 id 的节点。
- * 4. std::pair<T, int> top()，取得堆顶元素，pair 的第一个元素为值，第二个元素为其编号，即 id。
- * 5. std::pair<T, int> getById(int id)，返回编号为 id 的节点情况。
- * 6. void modify(int id, const T &v)，修改编号为 id 的节点的值。
+ * 1. int push(const T &v)，新增一个值为 v 的节点入堆，返回该新增节点的编号。
+ * 2. T top()，返回栈顶节点所存储的数据。
+ * 3. T get(const int id)，返回编号为 id 的节点所存储的数据。
+ * 4. void modify(const int id, const T &data)，修改编号为 id 的节点的数据为 data。
+ * 5. int pop()，弹出堆顶的节点，并返回它的编号。
+ * 6. void popById(const int id)，弹出编号为 id 的节点。
+ * 7. bool empty()，检查堆是否为空。
  *
  * 特别说明：
- * 模板 Compare = std::less<> 是大顶堆，Compare = std::greater<> 是小顶堆
+ * 模板 T 是数据类型，Compare 是重载了 bool operator()(const T &t1, const T &t2) 的类。
+ * （示例：Compare = std::less<> 是大顶堆，Compare = std::greater<> 是小顶堆）
  */
 
 #include <bits/stdc++.h>
@@ -19,65 +21,71 @@
 template <typename T, typename Compare = std::less<>>
 class PriorityQueue {
 public:
-    std::vector<std::pair<T, int>> ele;
-    std::vector<int> trace;
+    explicit PriorityQueue(Compare compare = Compare()) : ele(std::vector<Elem>(0)), trace(std::vector<int>(0)),
+    compare(compare), nextId(0), avai(std::vector<int>(0)) {}
 
-    explicit PriorityQueue() = default;
+    int push(const T &data) {
+        const int id = newId();
+        expandTraceIfNeeded(id);
 
-    void push(int id, const T &v) {
-        expandTrace(id);
+        ele.push_back({data, id});
+        trace[id] = ele.size() - 1;
+        adjust(ele.size() - 1);
 
-        if (trace[id] != -1) {
-            modify(id, v);
-            return;
+        return id;
+    }
+
+    T top() {
+        assert(!ele.empty());
+
+        return ele[0].data;
+    }
+
+    T get(const int id) {
+        assert(trace.size() > id && trace[id] != -1);
+
+        const int heapId = trace[id];
+        return ele[heapId].data;
+    }
+
+    void modify(const int id, const T &data) {
+        assert(trace.size() > id && trace[id] != -1);
+
+        const int heapId = trace[id];
+        ele[heapId].data = data;
+        adjust(heapId);
+    }
+
+    int pop() {
+        assert(!ele.empty());
+
+        const int id = ele[0].id;
+        ele[0] = ele[ele.size() - 1];
+        trace[id] = -1;
+        ele.pop_back();
+        if (!ele.empty()) {
+            trace[ele[0].id] = 0;
+            adjust(0);
         }
 
-        trace[id] = ele.size();
-        ele.emplace_back(v, id);
-        up(ele.size() - 1);
+        avai.push_back(id);
+
+        return id;
     }
 
-    void pop() {
-        assert(!ele.empty());
+    void popById(const int id) {
+        assert(trace.size() > id && trace[id] != -1);
 
-        trace[ele[ele.size() - 1].second] = 0;
-        trace[ele[0].second] = -1;
-        std::swap(ele[0], ele[ele.size() - 1]);
-        ele.pop_back();
-        adjust(0);
-    }
-
-    void popById(int id) {
-        assert(id < trace.size() && trace[id] != -1);
-
-        int heapId = trace[id];
-        trace[ele[ele.size() - 1].second] = trace[id];
+        const int heapId = trace[id];
+        ele[heapId] = ele[ele.size() - 1];
         trace[id] = -1;
-        std::swap(ele[heapId], ele[ele.size() - 1]);
         ele.pop_back();
-        adjust(heapId);
-    }
+        if (ele.size() != heapId) {
+            trace[ele[heapId].id] = heapId;
+            adjust(heapId);
+        }
 
-    std::pair<T, int> getById(int id) {
-        assert(id < trace.size() && trace[id] != -1);
-
-        return ele[trace[id]];
-    }
-
-    std::pair<T, int> top() {
-        assert(!ele.empty());
-
-        return ele[0];
-    }
-
-    void modify(int id, const T &v) {
-        int heapId = trace[id];
-        ele[heapId].first = v;
-        adjust(heapId);
-    }
-
-    int size() {
-        return ele.size();
+        avai.push_back(id);
     }
 
     bool empty() {
@@ -85,46 +93,76 @@ public:
     }
 
 private:
-    void up(int heapId) {
-        int cur = heapId;
-        while (cur > 0) {
-            int fa = (cur - 1) >> 1;
-            if (!Compare()(ele[fa], ele[cur])) { break; }
+    struct Elem {
+        T data;
+        int id;
+    };
 
-            int faId = ele[fa].second, curId = ele[cur].second;
-            trace[faId] = cur, trace[curId] = fa;
+    std::vector<Elem> ele;
+    std::vector<int> trace;
 
-            std::swap(ele[fa], ele[cur]);
-            cur = fa;
-        }
+    Compare compare;
+
+    int nextId;
+    std::vector<int> avai;
+
+    void adjust(const int heapId) {
+        down(heapId);
+        up(heapId);
     }
 
     void down(int heapId) {
-        int cur = heapId;
-        while (cur * 2 + 1 < ele.size()) {
-            int candidate = cur * 2 + 1, rChild = cur * 2 + 2;
-            if (rChild < ele.size() && !Compare()(ele[rChild], ele[candidate])) {
-                candidate = rChild;
+        const int n = static_cast<int>(ele.size());
+        while (true) {
+            const int child1 = heapId * 2 + 1, child2 = child1 + 1;
+            if (child1 >= n) {
+                break;
             }
 
-            if (!Compare()(ele[cur], ele[candidate])) { break; }
+            int betterChild = child1;
+            if (child2 < n && compare(ele[child1].data, ele[child2].data)) {
+                betterChild = child2;
+            }
 
-            int curId = ele[cur].second, candidateId = ele[candidate].second;
-            trace[curId] = candidate, trace[candidateId] = cur;
+            if (!compare(ele[heapId].data, ele[betterChild].data)) {
+                break;
+            }
 
-            std::swap(ele[cur], ele[candidate]);
-            cur = candidate;
+            trace[ele[heapId].id] = betterChild;
+            trace[ele[betterChild].id] = heapId;
+            std::swap(ele[betterChild], ele[heapId]);
+            heapId = betterChild;
         }
     }
 
-    void adjust(int heapId) {
-        up(heapId);
-        down(heapId);
+    void up(int heapId) {
+        while (heapId) {
+            const int fa = (heapId - 1) / 2;
+            if (!compare(ele[fa].data, ele[heapId].data)) {
+                break;
+            }
+
+            trace[ele[heapId].id] = fa;
+            trace[ele[fa].id] = heapId;
+            std::swap(ele[fa], ele[heapId]);
+            heapId = fa;
+        }
     }
 
-    void expandTrace(int maxId) {
-        if (maxId >= trace.size()) {
-            trace.resize(maxId + 1, -1);
+    int newId() {
+        if (!avai.empty()) {
+            const int id = avai.back();
+            avai.pop_back();
+            return id;
+        }
+
+        const int id = nextId++;
+        return id;
+    }
+
+    void expandTraceIfNeeded(const int id) {
+        if (trace.size() <= id) {
+            trace.resize(id + 1, -1);
         }
     }
 };
